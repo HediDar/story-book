da4fbcc3478dd0363135c1a81cdfdd05d99a0534
unknown <darg.medhed@gmail.com>
2020-09-22 10:25:39 +0100

    last push local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import AddToDO from "./components/AddToDo";
 import ToDosAndDones from "./components/ToDosAndDones";
 import "./App.css";
@@ -9,13 +16,20 @@ class App extends Component {
     this.increment = 0;
     this.state = {
       tasks: [],
+      tasksToShow: [],
+      inAll: 1,
+      inActive: 0,
+      inCompleted: 0,
     };
+    this.colorActif = "secondary";
+    this.colorAll = "primary";
+    this.colorCompleted = "secondary";
   }
 
   componentDidMount() {
     if (localStorage.getItem("tasksInLocalStorage")) {
       const data = JSON.parse(localStorage.getItem("tasksInLocalStorage"));
-      if (data.length > 0) this.setState({ tasks: data });
+      if (data.length > 0) this.setState({ tasks: data, tasksToShow: data });
     }
 
     if (localStorage.getItem("incrementInLocalStorage")) {
@@ -36,6 +50,9 @@ class App extends Component {
   // when we click first on add button
   handleAddToDone = (text, textArea, radio) => {
     const { tasks } = this.state;
+    const { inAll } = this.state;
+    const { inCompleted } = this.state;
+    const { inActive } = this.state;
     const data = tasks;
     if (radio.localeCompare("notImportant") === 0)
       data.push({
@@ -55,14 +72,20 @@ class App extends Component {
       });
 
     this.increment += 1;
-
     this.setState({ tasks: data });
+
+    if (inAll === 1) this.setState({ tasksToShow: data });
+    else if (inActive === 1) this.activeButtonClick();
+    else if (inCompleted === 1) this.completedButtonClick();
   };
 
   // when we click on delete task
 
   handleOnDoneTask = (value) => {
     const { tasks } = this.state;
+    const { inAll } = this.state;
+    const { inCompleted } = this.state;
+    const { inActive } = this.state;
     const data = tasks;
     let k = -1;
     data.forEach((el) => {
@@ -72,9 +95,15 @@ class App extends Component {
       }
     });
     this.setState({ tasks: data });
+    if (inAll === 1) this.setState({ tasksToShow: data });
+    else if (inActive === 1) this.activeButtonClick();
+    else if (inCompleted === 1) this.completedButtonClick();
   };
 
   handleonDeleteDone = (value) => {
+    const { inActive } = this.state;
+    const { inCompleted } = this.state;
+    const { inAll } = this.state;
     const { tasks } = this.state;
 
     const data = [];
@@ -85,25 +114,94 @@ class App extends Component {
         data.push(el);
       }
     });
-    this.setState({ tasks: data });
+
+    this.setState({ tasks: data }, () => {
+      if (inAll === 1) this.setState({ tasksToShow: tasks });
+      else if (inActive === 1) this.activeButtonClick();
+      else if (inCompleted === 1) this.completedButtonClick();
+    });
   };
 
   handleonImportant = (value) => {
     const { tasks } = this.state;
+    const { inAll } = this.state;
+    const { inActive } = this.state;
+    const { inCompleted } = this.state;
 
     const actualTasks = tasks;
     actualTasks.forEach((el, index) => {
       if (el.id === value) {
-        if (el.important === 0) actualTasks[index].important = 1;
-        else actualTasks[index].important = 0;
+        if (el.important === 1) actualTasks[index].important = 0;
+        else if (el.important === 0) actualTasks[index].important = 1;
+      }
+    });
+    this.setState({ tasks: actualTasks });
+    if (inAll === 1) this.setState({ tasksToShow: actualTasks });
+    else if (inActive === 1) this.activeButtonClick();
+    else if (inCompleted === 1) this.completedButtonClick();
+  };
+
+  allButtonClick = () => {
+    const { tasks } = this.state;
+    this.colorActif = "secondary";
+    this.colorAll = "primary";
+    this.colorCompleted = "secondary";
+    this.setState({
+      tasksToShow: tasks,
+      inAll: 1,
+      inActive: 0,
+      inCompleted: 0,
+    });
+  };
+
+  activeButtonClick = () => {
+    const { tasks } = this.state;
+    const myLoopData = tasks;
+    const toUpdataTasksToShow = [];
+
+
+    myLoopData.forEach((el) => {
+      if (el.done === 0) {
+        toUpdataTasksToShow.push(el);
       }
     });
 
-    this.setState({ tasks: actualTasks });
+    this.colorActif = "primary";
+    this.colorAll = "secondary";
+    this.colorCompleted = "secondary";
+
+    this.setState({
+      tasksToShow: toUpdataTasksToShow,
+      inAll: 0,
+      inActive: 1,
+      inCompleted: 0,
+    });
+  };
+
+  completedButtonClick = () => {
+    const { tasks } = this.state;
+    const myLoopData = tasks;
+    const toUpdataTasksToShow = [];
+
+    myLoopData.forEach((el) => {
+      if (el.done === 1) {
+        toUpdataTasksToShow.push(el);
+      }
+    });
+    this.colorActif = "secondary";
+    this.colorAll = "secondary";
+    this.colorCompleted = "primary";
+
+    this.setState({
+      tasksToShow: toUpdataTasksToShow,
+      inAll: 0,
+      inActive: 0,
+      inCompleted: 1,
+    });
   };
 
   render() {
-    const { tasks } = this.state;
+    const { tasksToShow } = this.state;
 
     return (
       <>
@@ -111,12 +209,52 @@ class App extends Component {
         <br />
         <br />
 
-        <ToDosAndDones
-          tasks={tasks}
-          onDoneTaskApp={this.handleOnDoneTask}
-          onDeleteTaskApp={this.handleonDeleteDone}
-          onMakeImportant={this.handleonImportant}
-        />
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                {tasksToShow.length}
+                {" items displayed"}{" "}
+              </TableCell>
+              <TableCell align="right"> </TableCell>
+              <TableCell align="right"> </TableCell>
+              <TableCell align="right">
+                <ButtonGroup aria-label="outlined secondary button group">
+                  <Button
+                    color={this.colorAll}
+                    onClick={() => this.allButtonClick()}
+                  >
+                    Show all
+                  </Button>
+                  <Button
+                    color={this.colorActif}
+                    onClick={() => this.activeButtonClick()}
+                  >
+                    Show active
+                  </Button>
+                  <Button
+                    color={this.colorCompleted}
+                    onClick={() => this.completedButtonClick()}
+                  >
+                    Show completed
+                  </Button>
+                </ButtonGroup>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell colSpan="4" component="th" scope="row">
+                <ToDosAndDones
+                  tasks={tasksToShow}
+                  onDoneTaskApp={this.handleOnDoneTask}
+                  onDeleteTaskApp={this.handleonDeleteDone}
+                  onMakeImportant={this.handleonImportant}
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </>
     );
   }
