b0e107d2b2d58286caad434eea7e7aa16ae7130d
unknown <darg.medhed@gmail.com>
2020-10-05 09:06:29 +0100

    commit setting up store for middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,27 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise-middleware";
 import tasksRecuder from "./reducers/tasksReducer";
 
 function configureStore(state) {
   return createStore(
     tasksRecuder,
+    applyMiddleware(promiseMiddleware),
     state,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 }
+
+// composeStoreWithMiddleware = applyMiddleware(
+//   promise,
+// )(createStore)
+
+// applyMiddleware(promise)(
+//   createStore(
+//     tasksRecuder,
+//     state,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//       window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default configureStore;
