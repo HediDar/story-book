a859a2b0554b6f73013a7bc99b6ebd72cbd6e5c7
unknown <darg.medhed@gmail.com>
2020-10-05 08:50:08 +0100

    first commit

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import uuid from "react-uuid";
 import {
   RadioGroup,
   FormControlLabel,
