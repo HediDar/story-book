99230cd3b202914ca5e6b527fe79494b7d4eec96
HediDar <darg.medhed@gmail.com>
2020-10-08 11:47:27 +0000

    Deploying to master from  @ 07a2c361734b1aa50681e36c24400624d5f54dce 🚀

---
+++ precache-manifest.a59fae56f733ecfb36af79e547d80920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65beeb3efd4f4e1919ba9e4540b9587b",
+    "url": "/story-book/index.html"
+  },
+  {
+    "revision": "f9383eaf71c483002607",
+    "url": "/story-book/static/css/main.75c945f9.chunk.css"
+  },
+  {
+    "revision": "b0a9e13541b65ffdfd9d",
+    "url": "/story-book/static/js/2.de0ee331.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/story-book/static/js/2.de0ee331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9383eaf71c483002607",
+    "url": "/story-book/static/js/main.8163996e.chunk.js"
+  },
+  {
+    "revision": "4e8b448a92e52cb210b1",
+    "url": "/story-book/static/js/runtime-main.64662495.js"
+  }
+]);
