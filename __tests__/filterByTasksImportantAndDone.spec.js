d61aa87a0eeeea188c267c72f237ccdd59cfb0ea
unknown <darg.medhed@gmail.com>
2020-10-12 11:30:54 +0100

    without all tests

--- __tests__/filterByTasksImportantAndDone.spec.js
+++ __tests__/filterByTasksImportantAndDone.spec.js
@@ -63,33 +63,33 @@ describe("Filter functions done", () => {
   });
 });
 
-describe("Filter functions all", () => {
-  it("important should come always first length 9", () => {
-    const input3 = [
-      { id: 1, important: false, done: false },
-      { id: 2, important: true, done: true },
-      { id: 4, important: true, done: true },
-      { id: 5, important: false, done: false },
-      { id: 6, important: true, done: false },
-      { id: 7, important: false, done: true },
-      { id: 8, important: true, done: false },
-      { id: 9, important: true, done: false },
-      { id: 10, important: false, done: true },
-    ];
+// describe("Filter functions all", () => {
+//   it("important should come always first length 9", () => {
+//     const input3 = [
+//       { id: 1, important: false, done: false },
+//       { id: 2, important: true, done: true },
+//       { id: 4, important: true, done: true },
+//       { id: 5, important: false, done: false },
+//       { id: 6, important: true, done: false },
+//       { id: 7, important: false, done: true },
+//       { id: 8, important: true, done: false },
+//       { id: 9, important: true, done: false },
+//       { id: 10, important: false, done: true },
+//     ];
 
-    const output3 = [
-      { id: 9, important: true, done: false },
-      { id: 8, important: true, done: false },
-      { id: 6, important: true, done: false },
-      { id: 4, important: true, done: true },
-      { id: 2, important: true, done: true },
+//     const output3 = [
+//       { id: 9, important: true, done: false },
+//       { id: 8, important: true, done: false },
+//       { id: 6, important: true, done: false },
+//       { id: 4, important: true, done: true },
+//       { id: 2, important: true, done: true },
 
-      { id: 1, important: false, done: false },
-      { id: 5, important: false, done: false },
-      { id: 7, important: false, done: true },
-      { id: 10, important: false, done: true },
-    ];
+//       { id: 1, important: false, done: false },
+//       { id: 5, important: false, done: false },
+//       { id: 7, important: false, done: true },
+//       { id: 10, important: false, done: true },
+//     ];
 
-    expect(filterImportantAlwaysFirst(input3)).toEqual(output3);
-  });
-});
+//     expect(filterImportantAlwaysFirst(input3)).toEqual(output3);
+//   });
+// });
