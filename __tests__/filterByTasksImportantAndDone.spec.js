ab53ffe4788e107dbbd47558efdf6ef4c68996bc
unknown <darg.medhed@gmail.com>
2020-10-12 11:21:52 +0100

    jest corrected

--- __tests__/filterByTasksImportantAndDone.spec.js
+++ __tests__/filterByTasksImportantAndDone.spec.js
@@ -2,7 +2,6 @@ import {
   displayOnlyDone,
   removeDone,
   filterImportantThenNotImportantThenDone,
-  filterDoneAlwaysLast,
   filterImportantAlwaysFirst,
 } from "../src/util/filters";
 
@@ -24,25 +23,7 @@ describe("Filter functions actif", () => {
   });
 });
 
-describe("Filter functions done", () => {
-  it("done should always be last", () => {
-    const input = [
-      { id: 2, important: true, done: true },
-      { id: 3, important: true, done: false },
-      { id: 1, important: false, done: false },
-      { id: 4, important: false, done: true },
-    ];
 
-    const output = [
-      { id: 1, important: false, done: false },
-      { id: 3, important: true, done: false },
-      { id: 4, important: false, done: true },
-      { id: 2, important: true, done: true },
-    ];
-
-    expect(filterDoneAlwaysLast(input)).toEqual(output);
-  });
-});
 
 describe("Filter functions final filter", () => {
   it("important +!done, not important+!done, then done", () => {
@@ -85,42 +66,7 @@ describe("Filter functions done", () => {
 });
 
 describe("Filter functions all", () => {
-  it("important should come always first length 4", () => {
-    const input = [
-      { id: 1, important: false, done: false },
-      { id: 2, important: true, done: true },
-      { id: 3, important: true, done: false },
-      { id: 4, important: false, done: true },
-    ];
-
-    const output = [
-      { id: 2, important: true, done: true },
-      { id: 3, important: true, done: false },
-      { id: 1, important: false, done: false },
-      { id: 4, important: false, done: true },
-    ];
-    expect(filterImportantAlwaysFirst(input)).toEqual(output);
-  });
-  it("important should come always first length 6", () => {
-    const input2 = [
-      { id: 1, important: true, done: true },
-      { id: 2, important: true, done: true },
-      { id: 3, important: false, done: false },
-      { id: 4, important: false, done: true },
-      { id: 5, important: true, done: false },
-      { id: 6, important: false, done: true },
-    ];
-
-    const output2 = [
-      { id: 1, important: true, done: true },
-      { id: 2, important: true, done: true },
-      { id: 5, important: true, done: false },
-      { id: 3, important: false, done: false },
-      { id: 4, important: false, done: true },
-      { id: 6, important: false, done: true },
-    ];
-    expect(filterImportantAlwaysFirst(input2)).toEqual(output2);
-  });
+ 
   it("important should come always first length 9", () => {
     const input3 = [
       { id: 1, important: false, done: false },
