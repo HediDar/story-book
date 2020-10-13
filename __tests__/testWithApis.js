68b61f679a1bc294f69c692e80ce94dba99aa193
unknown <darg.medhed@gmail.com>
2020-10-13 15:55:51 +0100

    commit on delete

--- __tests__/testWithApis.js
+++ __tests__/testWithApis.js
@@ -8,72 +8,72 @@ import {
 } from "../src/domain/myAPIS";
 
 jest.mock("axios");
+const apiUrl = "https://my-front-end-to-do.herokuapp.com/api/tasks";
+
 
 describe("getAllTasks", () => {
   it("fetches successfully all tasks data from an API", async () => {
-    const data = [
-      {
-        _id: "5f7c61ef72e7de0017f59ce6",
-        name: "important task",
-        description: "",
-        important: true,
-        done: true,
+    const data = {
+      data: {
+        tasks: [
+          {
+            _id: "5f7c61ef72e7de0017f59ce6",
+            name: "important task",
+            description: "",
+            important: true,
+            done: true,
+          },
+          {
+            _id: "5f7d9c19fffee20017541c25",
+            name: "Test from Bouba",
+            description: "This is just a test",
+            important: false,
+            done: true,
+          },
+        ],
       },
+    };
 
-      {
-        _id: "5f7d9c19fffee20017541c25",
-        name: "Test from Bouba",
-        description: "This is just a test",
-        important: false,
-        done: true,
-      },
-    ];
-
-    axios.get.mockImplementationOnce(() => Promise.resolve(data));
-
+    axios.get.mockResolvedValue(data);
     await expect(getAllTasks()).resolves.toEqual(data);
+    expect(axios.get).toHaveBeenCalledWith(apiUrl);
   });
 
   it("fetches erroneously tasks from my API", async () => {
     const errorMessage = "Network Error";
 
-    axios.get.mockImplementationOnce(() =>
-      Promise.reject(new Error(errorMessage))
-    );
+    axios.get.mockRejectedValue(new Error(errorMessage));
 
     await expect(getAllTasks()).rejects.toThrow(errorMessage);
+    expect(axios.get).toHaveBeenCalledWith(apiUrl);
   });
 });
 
 describe("addTask", () => {
   it("adds successfully a task data with an API", async () => {
     const data = {
-      _id: "5f7c61ef72e7de0017f59ce6",
       name: "important task",
       description: "",
       important: true,
       done: true,
     };
-
-    axios.post.mockImplementationOnce(() => Promise.resolve(data));
-
+    axios.post.mockResolvedValue(data);
     await expect(addTask(data)).resolves.toEqual(data);
+    expect(axios.post).toHaveBeenCalledWith(apiUrl, data);
   });
 
   it("adds erroneously a task with my API", async () => {
     const errorMessage = "Network Error";
     const data = {
-      _id: "5f7c61ef72e7de0017f59ce6",
       name: "important task",
       description: "",
       important: true,
       done: true,
     };
-    axios.post.mockImplementationOnce(() =>
-      Promise.reject(new Error(errorMessage))
-    );
+    axios.post.mockRejectedValue(new Error(errorMessage));
 
     await expect(addTask(data)).rejects.toThrow(errorMessage);
+    expect(axios.post).toHaveBeenCalledWith(apiUrl, data);
   });
 });
 
@@ -81,19 +81,20 @@ describe("deleteTask", () => {
   it("delete successfully a task with id with an API", async () => {
     const id = "5f7c61ef72e7de0017f59ce6";
 
-    axios.delete.mockImplementationOnce(() => Promise.resolve(id));
-
+    axios.delete.mockResolvedValue(id);
     await expect(deleteTask({ id })).resolves.toEqual(id);
+    expect(axios.delete).toHaveBeenCalledWith(apiUrl + "?_id=" + id);
   });
 
   it("deletes erroneously a task with id with my API", async () => {
     const errorMessage = "Network Error";
     const id = "5f7c61ef72e7de0017f59ce6";
-    axios.delete.mockImplementationOnce(() =>
-      Promise.reject(new Error(errorMessage))
-    );
 
+    axios.delete.mockRejectedValue(new Error(errorMessage));
     await expect(deleteTask({ id })).rejects.toThrow(errorMessage);
+    expect(axios.delete).toHaveBeenCalledWith(apiUrl + "?_id=" + id);
+
+
   });
 });
 
